266f9b9b740fa48bbc71c8bd5f5fc83e70b7ceb8
“xishuianfeng” <xishuianfeng@gmail.com>
2023-08-08 16:25:07 +0800

    打开时自动加载playlistLocation

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,6 +1,14 @@
-import { FC, PropsWithChildren } from "react"
+import { FC, PropsWithChildren, useEffect } from "react"
+import usePlaylistStore from "./store/playlistStore"
+import { useAsyncEffect } from 'ahooks'
 
 const App: FC<PropsWithChildren> = ({ children }) => {
+  const playlistStore = usePlaylistStore()
+  useAsyncEffect(async () => {
+    const { playlistLocations: latestPlaylistLocations } =
+      await window.api.fileIpc.emitGetPlaylistLocations()
+    playlistStore.setPlaylistLocations(latestPlaylistLocations)
+  }, [])
   return <div id="container">{children}</div>
 }
 
